2cf63504f448e9a804f2d404ad131c476efafd99
asim <88264839+buffbahun@users.noreply.github.com>
2022-12-05 12:53:10 +0545

    Enum write

---
+++ EntityTypes.cs
@@ -0,0 +1,15 @@
+public enum EntityTypes
+	{
+		[Display(Name = "Vendor", Description = "Vendor")]
+		vendor = 0,
+        [Display(Name = "Customer", Description = "Customer")]
+        customer,
+        [Display(Name = "Employee", Description = "Employee")]
+        employee,
+        [Display(Name = "Item", Description = "Item")]
+        item,
+        [Display(Name = "Unit", Description = "Unit")]
+        unit,
+        [Display(Name = "Item Category", Description = "Item Category")]
+        item_category,
+    }
